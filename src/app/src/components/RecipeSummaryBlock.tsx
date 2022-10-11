1de22b95219b2485138e76359c09d60090bf8707
Bobby <antonin.gontier@insa-rennes.fr>
2022-10-11 02:55:52 +0200

    Complete recipe summary component

--- src/app/src/components/RecipeSummaryBlock.tsx
+++ src/app/src/components/RecipeSummaryBlock.tsx
@@ -1,10 +1,22 @@
 import { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { RecipeSummary } from '../types/recipes';
+import './RecipeSummaryBlock.scss';
 
 class RecipeSummaryBlock extends Component<{ recipe: RecipeSummary, recipeId: string }, {}> {
   render() {
     return (
-      <li>{this.props.recipe.name}</li>
+      <li>
+        <Link to="#">
+          <Card>
+            <Card.Img variant="top" src={process.env.PUBLIC_URL + "/placeholder.jpg"} />
+            <Card.Body className="recipeName">
+              <Card.Title>{this.props.recipe.name}</Card.Title>
+            </Card.Body>
+          </Card>
+        </Link>
+      </li>
     )
   }
 }
